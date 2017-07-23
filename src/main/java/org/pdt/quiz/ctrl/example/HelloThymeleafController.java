86ded548f56d00a0961c99e03c85fea8a89af7dd
sun1093 <sun1093@gmail.com>
2017-07-24 00:03:53 +0800

    add thymeleaf request and if tag

--- src/main/java/org/pdt/quiz/ctrl/example/HelloThymeleafController.java
+++ src/main/java/org/pdt/quiz/ctrl/example/HelloThymeleafController.java
@@ -4,21 +4,22 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Thymeleaf的設計範本<br/>
+ * 
  * @author Orange
  *
  */
 @Controller
 public class HelloThymeleafController {
 
-
     @RequestMapping("/welcome")
     public String welcome(Map<String, Object> model) {
         model.put("message", "Hello World!");
         return "welcome";
     }
+
+    
 
 }
--- src/main/java/org/pdt/quiz/ctrl/test/TestController.java
+++ src/main/java/org/pdt/quiz/ctrl/test/TestController.java
@@ -1,32 +1,62 @@
 package org.pdt.quiz.ctrl.test;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
 
 /**
  * Thymeleaf的設計範本<br/>
+ * 
  * @author Orange
  *
  */
 @Controller
 public class TestController {
 
-
-    @RequestMapping(path="/test", method=RequestMethod.POST)
-    public String test(Map<String, Object> model, HttpServletRequest request) throws Exception {  
-        model.put("message", "Hello " + request.getParameter("input1"));
-        ServletInputStream inputStream = request.getInputStream();
+    @RequestMapping(path = "/test", method = {RequestMethod.POST, RequestMethod.GET})
+    public String test(Map<String, Object> model, HttpServletRequest request) throws Exception {
+        System.out.println(request.getMethod());
+        
+        if(RequestMethod.GET.toString().equals(request.getMethod())) {
+            
+            
+        }else {
+            List<String> l = new ArrayList<String>();
+            l.add("abc");
+            l.add("abc1");
+            
+            model.put("message", "Hello " + request.getParameter("input1"));
+            model.put("prods",  l);
+            
+            //ServletInputStream inputStream = request.getInputStream();
+        }
         
         return "test";
     }
+
+//    public void process(HttpServletRequest request, HttpServletResponse response, ServletContext servletContext,
+//            TemplateEngine templateEngine) throws IOException {
+//        // ProductService productService = new ProductService();
+//        // List<Product> allProducts = productService.findAll();
+//        
+//       
+//
+//        WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+//        ctx.setVariable("prods", l);
+//
+//        templateEngine.process("test", ctx, response.getWriter());
+//    }
 
 }
